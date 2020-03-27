73dc3c12611e3f82bd45f014f77bc0677a09e8d5
ABEYE-exe <62757651+ABEYE-exe@users.noreply.github.com>
2020-03-27 16:05:00 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "<";
 
-client.login("NjkzMDM4MjAwMzQzNzU2ODAw.Xn3Q7w.P-tV3IAHugpHC9QwLC_coQukANY");
+client.login(process.env.TOKEN);
 
 
 client.on("message", message =>{
